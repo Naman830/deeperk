a5ef25c0dbd5b80fcb454dfa9c3b61b34baa7c10
Naman <namansingla830@gmail.com>
2026-08-14 15:20:21 +0530

    add index.js file

---
+++ db/schema/profile/index.js
@@ -0,0 +1,10 @@
+const { socialLink } = require("./social-link");
+const { privacySettings } = require("./privacy-settings");
+const { pendingContactChangeTypeEnum, pendingContactChange } = require("./pending-contact-change");
+
+module.exports = {
+  socialLink,
+  privacySettings,
+  pendingContactChangeTypeEnum,
+  pendingContactChange,
+};
