2950c13ab9ff93393e8229eff85038cccba693af
Naman <namansingla830@gmail.com>
2026-08-14 15:26:42 +0530

    add index.js

---
+++ db/schema/call/index.js
@@ -0,0 +1,9 @@
+const { callKindEnum, callStatusEnum, call } = require("./call");
+const { callParticipant } = require("./call-participant");
+
+module.exports = {
+  callKindEnum,
+  callStatusEnum,
+  call,
+  callParticipant,
+};
